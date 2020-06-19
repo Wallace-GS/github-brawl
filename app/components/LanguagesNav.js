00f8491839b6a69b7303546595c1205f18a928be
Wallace-GS <wgsantos29@gmail.com>
2020-06-19 09:34:04 -0700

    adds navbar

---
+++ app/components/LanguagesNav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return (
+    <ul className="flex-center">
+      {languages.map((language, index) => (
+        <li key={index}>
+          <button
+            className="btn-clear nav-link"
+            style={
+              language === selected
+                ? {
+                    color: 'rgb(187, 46, 31)',
+                  }
+                : null
+            }
+            onClick={() => onUpdateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired,
+};
