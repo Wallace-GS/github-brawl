6cf0a553e31a9292a0eacfcfc3662f0d9b160d07
Wallace-GS <wgsantos29@gmail.com>
2020-07-05 14:55:36 -0700

    setting up context for light/dark theme

--- app/index.js
+++ app/index.js
@@ -3,13 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Popular from './pages/Popular';
 import Brawl from './pages/Brawl';
+import { ThemeProvider } from './contexts/theme';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light',
+        }));
+      },
+    };
+  }
+
   render() {
     return (
-      <div className="container">
-        <Brawl />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Brawl />
+        </div>
+      </ThemeProvider>
     );
   }
 }
